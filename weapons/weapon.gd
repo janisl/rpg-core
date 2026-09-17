2231f552eb56d0ede1344452a4438e04e6cfa7d1
Janis Legzdins <legzdinsjanis@gmail.com>
2026-09-17 13:07:46 +0300

    Add weapon resource.

---
+++ weapons/weapon.gd
@@ -0,0 +1,8 @@
+class_name Weapon
+extends Resource
+
+@export var weapon_name := "Weapon"
+@export var damage := 25.0
+@export var max_amo := 12
+@export var weapon_model: PackedScene
+@export var weapon_position := Vector3.ZERO
