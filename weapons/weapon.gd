2e0c3ede7fe1f2e4d40ed547d9a1b39e8f1cdf3e
Janis Legzdins <legzdinsjanis@gmail.com>
2026-09-17 20:25:18 +0300

    Fix property name

--- weapons/weapon.gd
+++ weapons/weapon.gd
@@ -3,7 +3,7 @@ extends Resource
 
 @export var weapon_name := "Weapon"
 @export var damage := 25.0
-@export var max_amo := 12
+@export var max_ammo := 12
 @export var fire_rate := 2.0
 @export var is_automatic := false
 @export var range := 25.0
